cacc0760becd03dfe74a4d4cf37031b65f1f0a15
Frances McMullin <francesmcmullin@protonmail.com>
2025-11-19 13:14:33 +0000

    Update for AR 7.2.3 - connection pools hurray

--- app/app.rb
+++ app/app.rb
@@ -13,7 +13,7 @@ module ActivateAdmin
     end
 
     def initialize
-      unless ActiveRecord::Base.connection.active?
+      unless ActiveRecord::Base.connection.active? || ActiveRecord::Base.connection_pool.active_connection?
         if ENV['DATABASE_URL'] and ENV['DATABASE_SCHEMA']
           ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
           ActiveRecord::Base.connection.schema_search_path = ENV['DATABASE_SCHEMA']
